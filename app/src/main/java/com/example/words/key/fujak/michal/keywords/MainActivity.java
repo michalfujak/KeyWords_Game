8ec8f94e98db97db394f05e1a46f3fc98d668bff
= <michalfujak005@gmail.com>
2018-10-17 01:00:36 +0200

    Create project [ game for android ]

---
+++ app/src/main/java/com/example/words/key/fujak/michal/keywords/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.words.key.fujak.michal.keywords;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 0:00 start
+    }
+}
